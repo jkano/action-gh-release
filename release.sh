fc8f4e9dbfd5b7b2255eb3a122fccf279cce4f0f
Jose Cano <joseph8814@hotmail.com>
2024-02-21 20:00:54 -0500

    Version v1.0.2

--- release.sh
+++ release.sh
@@ -8,10 +8,48 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
-git checkout -b releases/$1 # If this branch already exists, omit the -b flag
+# First push changes to our current branch so the current branch is updated
+git push
+current_branch=$(git branch --show-current)
+
+git rev-parse --abbrev-ref HEAD
+
+# Now we are ready to release
+if [ `git rev-parse --verify releases/$1 2>/dev/null` ]; then
+	echo ""
+else
+	echo "Creating branch $1"
+	git checkout -b releases/$1 # If this branch already exists, omit the -b flag
+fi
+
 rm -rf node_modules
-sed -i '/node_modules/d' .gitignore # Bash command that removes node_modules from .gitignore
-npm install --production
-git add node_modules -f .gitignore
-git commit -m node_modules
+
+## Backup current .gitignore
+mv .gitignore .gitignore_bak
+## Create new one without node_modules
+echo "__tests__/runner/*" >> .gitignore
+
+#npm install --production --silent
+npm install --silent
+npm run build
+
+## Replace new .gitignore with old one
+mv .gitignore_bak .gitignore
+
+## Add everything and commit
+git add .
+git commit -m "Version $1"
 git push origin releases/$1
+
+# Delete the remote tag
+git push --delete origin $1
+
+# Create the tag
+git tag -d $1
+git tag $1
+
+# Push
+git push --tags
+
+# Now return to your working branch
+git checkout $current_branch
